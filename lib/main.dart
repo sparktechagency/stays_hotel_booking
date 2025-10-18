ea49770fda810d0930b9ff8a931ed4a976fc32e7
Chironjit598 <chironjitchandraroy@gmail.com>
2025-10-18 15:56:07 +0600

    complete splash, onboarding and subscription screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:stays_hotel_booking/app.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await init();
+  runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+Future<void> init() async {
+  await Future.wait([
+   
+   
+  ]);
 }
